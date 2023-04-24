8fe4f8a77e668817fc317346715c2421c831ccc6
jobanjit-singh <singhjobanjit02@gmail.com>
2023-04-25 00:23:06 +0530

    Add Detail Activity Design is Ready

--- app/src/main/java/com/example/telecomm/AddDetails.java
+++ app/src/main/java/com/example/telecomm/AddDetails.java
@@ -1,14 +1,32 @@
 package com.example.telecomm;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class AddDetails extends AppCompatActivity {
-
+    ImageView fishImage,addDetailAddImage;
+    EditText addDetailNameEdit,addDetailNumberEdit;
+    Button addDetailNextBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_details);
+
+
+        addDetailAddImage = findViewById(R.id.addDetailAddImageView);
+        addDetailNameEdit = findViewById(R.id.addDetailNameEditText);
+        addDetailNumberEdit = findViewById(R.id.addDetailNumberEditText);
+        addDetailNextBtn = findViewById(R.id.addDetailNextBtn);
+
+        fishImage = findViewById(R.id.addDetailFishImage);
+        Animation an = AnimationUtils.loadAnimation(AddDetails.this,R.anim.add_details_fish_image_anim);
+        fishImage.startAnimation(an);
     }
 }
