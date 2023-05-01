c454a72d7adfac0cbcd13fe2566b7a458747ccec
jobanjit-singh <singhjobanjit02@gmail.com>
2023-05-02 00:31:28 +0530

    Chat Done in Backend - Design Pending

--- app/src/main/java/com/example/telecomm/ChatFragment.java
+++ app/src/main/java/com/example/telecomm/ChatFragment.java
@@ -1,21 +1,27 @@
 package com.example.telecomm;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ChatFragment extends Fragment {
     ChatContactListDataModel UserData;
@@ -39,5 +45,32 @@ public class ChatFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ImageView chatFragImg = getView().findViewById(R.id.chatFragmentImg);
+        TextView chatFragText = getView().findViewById(R.id.chatFragmentText);
+        String user = FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber();
+        FirebaseDatabase.getInstance().getReference("chat").child(user).child("Profile Image").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Glide.with(getContext()).load(Uri.parse(snapshot.getValue(String.class))).into(chatFragImg);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), "Error in fetching Profile Image", Toast.LENGTH_SHORT).show();
+                Glide.with(getContext()).load(R.drawable.telecomm).into(chatFragImg);
+            }
+        });
+        FirebaseDatabase.getInstance().getReference("chat").child(user).child("User Name").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                chatFragText.setText(new EncryptDecryptData().dataDecryption(snapshot.getValue(String.class)));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), "Error in fetching User Name", Toast.LENGTH_SHORT).show();
+                chatFragText.setText(R.string.app_name);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/telecomm/SenderReceiverChatFragment.java
+++ app/src/main/java/com/example/telecomm/SenderReceiverChatFragment.java
@@ -1,20 +1,36 @@
 package com.example.telecomm;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class SenderReceiverChatFragment extends Fragment {
-    String UserName,ProfileImage,UserNumber;
-    ImageView SendReceiveBackBtn;
+    String ReceiverUserName,ReceiverProfileImage,ReceiverUserNumber,SenderUserNumber;
+    ImageView SendReceiveBackBtn,SendReceiveProfileImage,SendReceiveSendBtn;
+    TextView SendReceiveUserName;
+    EditText SendReceiveMsgEdittext;
+    RecyclerView SendReceiveRecyclerView;
+
     SendReceiveBack back;
     @Nullable
     @Override
@@ -26,15 +42,67 @@ public class SenderReceiverChatFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        UserName = getArguments().getString("UserName");
-        UserNumber = getArguments().getString("UserNumber");
-        ProfileImage = getArguments().getString("ProfileImage");
+        SenderUserNumber= FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber();
+        ReceiverUserName = getArguments().getString("UserName");
+        ReceiverUserNumber = getArguments().getString("UserNumber");
+        ReceiverProfileImage = getArguments().getString("ProfileImage");
 
         SendReceiveBackBtn = getView().findViewById(R.id.SendReceiveBackButton);
+        SendReceiveProfileImage = getView().findViewById(R.id.SendReceiveProfileImage);
+        SendReceiveUserName = getView().findViewById(R.id.SendReceiveUserName);
+        SendReceiveMsgEdittext = getView().findViewById(R.id.SendReceiveMsgEditText);
+        SendReceiveSendBtn = getView().findViewById(R.id.SendReceiveSendBtn);
+        SendReceiveRecyclerView = getView().findViewById(R.id.SendReceiveRecyclerView);
+
+        Glide.with(getContext()).load(Uri.parse(ReceiverProfileImage)).into(SendReceiveProfileImage);
+        SendReceiveUserName.setText(ReceiverUserName);
         SendReceiveBackBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 back.onclickBackBtn();
+            }
+        });
+
+        SendReceiveSendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(SendReceiveMsgEdittext.getText().toString().isEmpty()){
+                    Toast.makeText(getContext(), "Enter the Message", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    HashMap<String,String> Msg = new HashMap<>();
+                    String msg = new EncryptDecryptData().dataEncryption(SendReceiveMsgEdittext.getText().toString());
+                    Msg.put("Type","Send");
+                    Msg.put("Message",msg);
+                    FirebaseDatabase.getInstance().getReference("chat").child(SenderUserNumber).child("chats").child(ReceiverUserNumber).push()
+                            .setValue(Msg)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    SendReceiveMsgEdittext.setText("");
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                    Msg.put("Type","Receive");
+                    Msg.put("Message",msg);
+                    FirebaseDatabase.getInstance().getReference("chat").child(ReceiverUserNumber).child("chats").child(SenderUserNumber).push()
+                            .setValue(Msg)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    SendReceiveMsgEdittext.setText("");
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                }
             }
         });
 
